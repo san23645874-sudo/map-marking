e5cdb26d854a3eb95bd0bbe346bd50b0d46eb61f
san23645874-sudo <san23645874@gmail.com>
2026-07-19 18:44:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,221 @@
+var mahawaliGaga = document.getElementById("mahawali_gaga")
+var kanagarayanAru = document.getElementById("kanagarayan_aru")
+var maoya = document.getElementById("ma_oya")
+var yanOya = document.getElementById("yan_oya")
+var maduruOya = document.getElementById("maduru_oya")
+var galOya = document.getElementById("gal_oya")
+var kubukkanOya = document.getElementById("kubukkqn_oya")
+var manikGaga= document.getElementById("manik_gaga")
+var kiridiOya = document.getElementById("kiridi_oya")
+var walaweGaga = document.getElementById("walawe_gaga")
+var nilwalaGaga = document.getElementById("nilwala_gaga")
+var ginGaga = document.getElementById("gin_gaga")
+var kaluGaga = document.getElementById("kalu_gaga")
+var kalaniGaga = document.getElementById("kalani_gaga")
+var mahaOya = document.getElementById("maha_oya")
+var daduruOya = document.getElementById("dauru_oya")
+var kalaOya = document.getElementById("kala_oya")
+var modharagamAru = document.getElementById("modharagam_aru")
+var malwathuOya = document.getElementById("malwathu_oya")
+
+
+
+document.getElementById("button").onclick = function() {
+  console.log("Button clicked!");
+  console.log(mahawaliGaga.value);
+  
+  if (mahawaliGaga.value.toLowerCase() === "mahawali gaga") {
+      mahawaliGaga.style.border = "2px solid green";
+
+  } else {
+      mahawaliGaga.style.border = "2px solid red";
+
+  }
+
+  
+  if (kanagarayanAru.value.toLowerCase() === "kanagarayan aru") {
+      kanagarayanAru.style.border = "2px solid green";
+
+  } else {
+      kanagarayanAru.style.border = "2px solid red";
+
+  }
+
+
+  
+  if (maoya.value.toLowerCase() === "ma oya") {
+      maoya.style.border = "2px solid green";
+
+  } else {
+      maoya.style.border = "2px solid red";
+
+  }
+
+
+  
+  if (yanOya.value.toLowerCase() === "yan oya") {
+      yanOya.style.border = "2px solid green";
+
+  } else {
+      yanOya.style.border = "2px solid red";
+
+  }
+
+
+  
+  if (maduruOya.value.toLowerCase() === "maduru oya") {
+      maduruOya.style.border = "2px solid green";
+
+  } else {
+      maduruOya.style.border = "2px solid red";
+
+  }
+
+
+  
+  if (galOya.value.toLowerCase() === "gal oya") {
+      galOya.style.border = "2px solid green";
+
+  } else {
+      galOya.style.border = "2px solid red";
+
+  }
+
+
+  
+  if (kubukkanOya.value.toLowerCase() === "kubukkan oya") {
+      kubukkanOya.style.border = "2px solid green";
+
+  } else {
+      kubukkanOya.style.border = "2px solid red";
+
+  }
+
+
+  
+  if (manikGaga.value.toLowerCase() === "manik gaga") {
+      manikGaga.style.border = "2px solid green";
+
+  } else {
+      manikGaga.style.border = "2px solid red";
+
+  }
+
+
+  
+  if (kiridiOya.value.toLowerCase() === "kiridi oya") {
+      kiridiOya.style.border = "2px solid green";
+
+  } else {
+      kiridiOya.style.border = "2px solid red";
+
+  }
+
+
+  
+  if (walaweGaga.value.toLowerCase() === "walawe gaga") {
+      walaweGaga.style.border = "2px solid green";
+
+  } else {
+      walaweGaga.style.border = "2px solid red";
+
+  }
+
+
+  
+  if (nilwalaGaga.value.toLowerCase() === "nilwala gaga") {
+      nilwalaGaga.style.border = "2px solid green";
+
+  } else {
+      nilwalaGaga.style.border = "2px solid red";
+
+  }
+
+
+  
+  if (ginGaga.value.toLowerCase() === "gin gaga") {
+      ginGaga.style.border = "2px solid green";
+
+  } else {
+      ginGaga.style.border = "2px solid red";
+
+  }
+
+
+  
+  if (kaluGaga.value.toLowerCase() === "kalu gaga") {
+      kaluGaga.style.border = "2px solid green";
+
+  } else {
+      kaluGaga.style.border = "2px solid red";
+
+  }
+
+
+  
+  if (kalaniGaga.value.toLowerCase() === "kalani gaga") {
+      kalaniGaga.style.border = "2px solid green";
+
+  } else {
+      kalaniGaga.style.border = "2px solid red";
+
+  }
+
+
+  
+  if (mahaOya.value.toLowerCase() === "maha oya") {
+      mahaOya.style.border = "2px solid green";
+
+  } else {
+      mahaOya.style.border = "2px solid red";
+
+  }
+
+
+  
+  if (daduruOya.value.toLowerCase() === "daduru oya") {
+      daduruOya.style.border = "2px solid green";
+
+  } else {
+      daduruOya.style.border = "2px solid red";
+
+  }
+
+
+  
+  if (kalaOya.value.toLowerCase() === "kala oya") {
+      kalaOya.style.border = "2px solid green";
+
+  } else {
+      kalaOya.style.border = "2px solid red";
+
+  }
+
+
+  
+  if (modharagamAru.value.toLowerCase() === "modharagam aru") {
+      modharagamAru.style.border = "2px solid green";
+
+  } else {
+      modharagamAru.style.border = "2px solid red";
+
+  }
+
+
+  
+  if (malwathuOya.value.toLowerCase() === "malwathu oya") {
+      malwathuOya.style.border = "2px solid green";
+
+  } else {
+      malwathuOya.style.border = "2px solid red";
+
+  }
+
+
+
+
+
+}
+  
+
+
